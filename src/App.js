b7f216f1a8a5ea6a56d81515b5864f9e545dd07c
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-04-18 17:02:42 +1000

    categories working

--- src/App.js
+++ src/App.js
@@ -44,7 +44,14 @@ class App extends Component {
       { key: 5, label: 'fan_count' },
       { key: 6, label: 'posts' },
     ],
-    postStatistics: '',
+    postStatistics: [
+      { key: 0, label: 'type' },
+      { key: 1, label: 'name' },
+      { key: 2, label: 'message' },
+      { key: 3, label: 'created_time' },
+      { key: 4, label: 'likes' },
+      { key: 5, label: 'comments' },
+    ],
     postID: '20531316728_10157104308481729',
     value: 0,
     newCategory: '',
@@ -71,19 +78,13 @@ class App extends Component {
     )
   }
 
-  handleDelete = data => () => {
-    const pageStatistics = [...this.state.pageStatistics]
-    const chipToDelete = pageStatistics.indexOf(data)
-    pageStatistics.splice(chipToDelete, 1)
-    this.setState({ pageStatistics })
-  }
-
   queryAPI() {
-    const { accessToken, companyName, postStatistics, postID, activeTab } = this.state
+    const { accessToken, companyName, postID, activeTab } = this.state
     const searchPage = activeTab === 0 ? true : false
 
-    // pull out pageStatistics from object
-    const pageStatistics = _.map(this.state.pageStatistics, x => x.label)
+    // pull out statistics from object
+    const pageStatistics = _.map(this.state.pageStatistics, x => x.label).join(",")
+    const postStatistics = _.map(this.state.postStatistics, x => x.label).join(",")
 
     const apiBase = searchPage ? `${companyName}?statistics=${pageStatistics}` : `post/${postID}?statistics=${postStatistics}`
 
@@ -103,18 +104,30 @@ class App extends Component {
     this.setState({ activeTab: value })
   }
 
-  addCategory = () => {
-    let pageStatistics = [...this.state.pageStatistics]
-    
+  handleDelete = (data, pageStatistics = true) => () => {
+    let statistics = [...pageStatistics ? this.state.pageStatistics : this.state.postStatistics]
+
+    const chipToDelete = statistics.indexOf(data)
+    statistics.splice(chipToDelete, 1)
+
+    const statKey = pageStatistics ? "pageStatistics" : "postStatistics"
+    this.setState({ [statKey]: statistics })
+  }
+
+  addCategory = (pageStatistics = true) => {
+    let statistics = [...pageStatistics ? this.state.pageStatistics : this.state.postStatistics]
+
     //generate new key from 0 .. 10000
     let key = Math.floor((Math.random() * 10000) + 1)
     // ensure the keyis unique
-    while (!_.isEmpty(_.filter(pageStatistics, stat => stat.key === key ))) {
+    while (!_.isEmpty(_.filter(statistics, stat => stat.key === key ))) {
       key = Math.floor((Math.random() * 10000) + 1)
     }
-    
-    pageStatistics.push({ key, label: this.state.newCategory })
-    this.setState({ pageStatistics })
+
+    statistics.push({ key, label: this.state.newCategory })
+
+    const statKey = pageStatistics ? "pageStatistics" : "postStatistics"
+    this.setState({ [statKey]: statistics })
 
   }
 
@@ -124,121 +137,137 @@ class App extends Component {
     const { classes } = this.props
 
     return (
-      <Grid container alignItems="center" spacing={0} direction="column" className={classes.root}>
-        <Grid item>
-          <Paper className={classes.root}>
-            <Tabs
-              value={this.state.activeTab}
-              onChange={this.updateTabs}
-              indicatorColor="primary"
-              textColor="primary"
-              fullWidth
-              centered
-            >
-              <Tab label="Query a Company" />
-              <Tab label="Query a Post" />
-            </Tabs>
-          </Paper>
+      <Grid container className={classes.root} direction="row">
+        <Grid item xs={12}>
+          <Grid container justify="center" direction="column">
+            <Grid item xs={12}>
 
+              <Paper>
+                <Tabs
+                  value={this.state.activeTab}
+                  onChange={this.updateTabs}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  fullWidth
+                  centered
+                >
+                  <Tab label="Query a Company" />
+                  <Tab label="Query a Post" />
+                </Tabs>
+              </Paper>
 
-          <TextField
-            label="Access Token"
-            name='accessToken'
-            className={classes.textField}
-            value={this.state.accessToken}
-            onChange={this.handleChange}
-            margin="normal"
-          />
+              <TextField
+                label="Access Token"
+                name='accessToken'
+                className={classes.textField}
+                value={this.state.accessToken}
+                onChange={this.handleChange}
+                margin="normal"
+              />
 
-          {this.state.activeTab === 0 ?
-            (
-              <div>
-                <TextField
-                  label="Company Name"
-                  name='companyName'
-                  className={classes.textField}
-                  value={this.state.companyName}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-
-                <br/>
-
-                <TextField
-                  label="Add Category"
-                  name="newCategory"
-                  className={classes.textField}
-                  value={this.state.newCategory}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-
-                <Button
-                  variant="raised"
-                  color="secondary"
-                  size="small"
-                  className={classes.button}
-                  onClick={() => this.addCategory()}>
-                  Add Category
-                </Button>
-
-                {this.state.pageStatistics.map(data =>
-                    <Chip
-                      key={data.key}
-                      label={data.label}
-                      onDelete={this.handleDelete(data)}
-                      className={classes.chip}
+              {this.state.activeTab === 0 ?
+                (
+                  <div>
+                    <TextField
+                      label="Company Name"
+                      name='companyName'
+                      className={classes.textField}
+                      value={this.state.companyName}
+                      onChange={this.handleChange}
+                      margin="normal"
                     />
-                )}
+
+                    <br/>
+
+                    <TextField
+                      label="Add Category"
+                      name="newCategory"
+                      className={classes.textField}
+                      value={this.state.newCategory}
+                      onChange={this.handleChange}
+                      margin="normal"
+                    />
+
+                    <Button
+                      variant="raised"
+                      color="secondary"
+                      size="small"
+                      className={classes.button}
+                      onClick={() => this.addCategory()}>
+                      Add Category
+                    </Button>
+
+                    {this.state.pageStatistics.map(data =>
+                        <Chip
+                          key={data.key}
+                          label={data.label}
+                          onDelete={this.handleDelete(data)}
+                          className={classes.chip}
+                        />
+                    )}
+
+                  </div>
+                )
+                :
+                (
+                  <div>
+                    <TextField
+                      label="Post ID"
+                      name='postID'
+                      className={classes.textField}
+                      value={this.state.postID}
+                      onChange={this.handleChange}
+                      margin="normal"
+                    />
+
+                    <br/>
+
+                    <TextField
+                      label="Add Category"
+                      name="newCategory"
+                      className={classes.textField}
+                      value={this.state.newCategory}
+                      onChange={this.handleChange}
+                      margin="normal"
+                    />
+
+                    <Button
+                      variant="raised"
+                      color="secondary"
+                      size="small"
+                      className={classes.button}
+                      onClick={() => this.addCategory(false)}>
+                      Add Category
+                    </Button>
+
+                    {this.state.postStatistics.map(data =>
+                        <Chip
+                          key={data.key}
+                          label={data.label}
+                          onDelete={this.handleDelete(data, false)}
+                          className={classes.chip}
+                        />
+                    )}
+
+                  </div>
+                )
+              }
+              <Button
+                variant="raised"
+                color="primary"
+                className={classes.button}
+                onClick={() => this.queryAPI()}>
+                Search
+              </Button>
 
 
-                {/* <TextField
-                  label="Page Statistics"
-                  name='pageStatistics'
-                  className={classes.textField}
-                  value={this.state.pageStatistics}
-                  onChange={this.handleChange}
-                  margin="normal"
-                /> */}
-              </div>
-            )
-            :
-            (
-              <div>
-                <TextField
-                  label="Post ID"
-                  name='postID'
-                  className={classes.textField}
-                  value={this.state.postID}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
+              {responseJSON ?
+                <JSONTree data={responseJSON} shouldExpandNode={(keyName, data) => keyName.includes("posts") ? false : true}/>
+                : null
+              }
 
-                <TextField
-                  label="Post Statistics"
-                  name='postStatistics'
-                  className={classes.textField}
-                  value={this.state.postStatistics}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-
-              </div>
-            )
-          }
-          <Button
-            variant="raised"
-            color="primary"
-            className={classes.button}
-            onClick={() => this.queryAPI()}>
-            Search
-          </Button>
-
-
-          {responseJSON ?
-            <JSONTree data={responseJSON} shouldExpandNode={(keyName, data) => keyName.includes("posts") ? false : true}/>
-            : null
-          }
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     )
