89c29db1b904d62aa24d38d805a0771e0aa2a023
Dominic Fung <domokun997@gmail.com>
2018-04-18 22:58:26 +1000

    Add break between category input and chips

--- src/App.js
+++ src/App.js
@@ -203,6 +203,8 @@ class App extends Component {
                       Add Category
                     </Button>
 
+                    <br/>
+
                     {this.state.pageStatistics.map(data =>
                         <Chip
                           key={data.key}
