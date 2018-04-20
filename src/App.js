20f67d9f18b0c601800f3aa4d6cb6e1936f4e232
William Watson <willw66@mac.com>
2018-04-20 20:37:05 +1000

    dates string stripped of time

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
     start_date: "",
     end_date: ""
   }
-  
+
   handleChange = (event, shouldReload = false) => {
     const target = event.target
     const value = target.type === 'checkbox' ? target.checked : target.value
@@ -97,8 +97,8 @@ class App extends Component {
     const searchPage = activeTab === 0 ? true : false
 
     //turn date to ISO
-    const start_date = (new moment(this.state.start_date)).toISOString()
-    const end_date = (new moment(this.state.end_date)).toISOString()
+    const start_date = (new moment(this.state.start_date)).seconds(0).milliseconds(0).toISOString().match(/(\d{4})-(\d{2})-(\d{2})/)[0]
+    const end_date = (new moment(this.state.end_date)).seconds(0).milliseconds(0).toISOString().match(/(\d{4})-(\d{2})-(\d{2})/)[0]
 
     // pull out statistics from object
     const pageStatistics = _.keys(_.pickBy(this.state.pageStatistics, (v, k) => v === true)).join(",")
@@ -106,9 +106,9 @@ class App extends Component {
 
     let apiBase = searchPage ? `${companyName}?statistics=${pageStatistics}` : `post/${postID}?statistics=${postStatistics}`
 
-    if (start_date && end_date) 
-      apiBase += `&start_date=${start_date}&end_date=${end_date}` 
-    
+    if (start_date && end_date)
+      apiBase += `&start_date=${start_date}&end_date=${end_date}`
+
     fetch(`https://unassigned-api.herokuapp.com/api/${apiBase}&access_token=${accessToken}`)
     .then((response) => {
       if (response.ok) {
@@ -264,4 +264,3 @@ class App extends Component {
 }
 
 export default withStyles(styles)(App)
-
